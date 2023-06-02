66d5b3f907486ee7aa03af7cb6eaf76dbb9193af
samba914 <sambadiarra2000@gmail.com>
2023-06-02 11:22:00 +0200

    edit

--- gcc.mk
+++ gcc.mk
@@ -1,15 +1,15 @@
-
-%.o : $.c
+%.o : %.c
 	gcc -fPIC ${CFLAGS} -c $< -o $@
 
 %.o : %.cpp
 	g++ -fPIC ${CPPFLAGS} -c $< -o $@
 
 ${EXE} : ${OBJS}
-	g++ ${OBJS} ${LDFLAGS} -o $@
+	g++ ${OBJS} ${LDFLAGS} -o $@ ${LIBS}
 
 ${LIBSO} : ${LIBSO_OBJS}
 	g++ -o $@ -shared ${LDFLAGS} $^ ${LIBS}
 
-${LIB} : $ ${LIB_OBJS}
+${LIB} : ${LIB_OBJS}
 	ar -cr $@ $^
+
