941b122487ab021a444a538d6d2293979437c2e5
samba914 <45944998+samba914@users.noreply.github.com>
2023-09-22 20:02:12 +0200

    Update test.py

--- composant_ECDSASignature/test.py
+++ composant_ECDSASignature/test.py
@@ -1,35 +1,10 @@
-import composant_ECDSASignature
-import re
+import composant_BIP39Converter as bip39
 
-# Remplacez ceci par une clé privée valide
-known_private_key = "4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0"
-known_message = "Hello, World!"
+converter = bip39.BIP39Converter()
 
-# Test de signature
-signer = composant_ECDSASignature.ECDSASignature()
-signer.Initialize(known_private_key)
-signature = signer.Sign(known_message)
+private_key = "4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0"
+mnemonic = converter.privateKeyToMnemonic(private_key)
+print(mnemonic)
 
-print("Signature :")
-print(signature)
-
-# Vérification de la longueur de la signature
-if len(signature) == 128:
-    print("Signature length test passed.")
-else:
-    print("Signature length test failed: Signature does not have the expected length.")
-
-# Vérification que la signature est en format hexadécimal
-if re.fullmatch(r'[0-9a-fA-F]*', signature):
-    print("Signature format test passed.")
-else:
-    print("Signature format test failed: Signature is not hexadecimal.")
-
-# Test avec une clé privée invalide
-try:
-    signer = composant_ECDSASignature.ECDSASignature()
-    signer.Initialize("INVALID_PRIVATE_KEY")
-except Exception:
-    print("Private key test passed: Exception correctly thrown for invalid private key.")
-else:
-    print("Private key test failed: No exception thrown for invalid private key.")
+recovered_key = converter.mnemonicToPrivateKey(mnemonic)
+print(recovered_key == private_key)  # Devrait afficher True
