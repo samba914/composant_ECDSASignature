1ba1811d6fc1d22b52c7cf4595db1aea782bf2f1
samba914 <45944998+samba914@users.noreply.github.com>
2023-09-22 21:00:08 +0200

    Update test.py

--- composant_ECDSASignature/test.py
+++ composant_ECDSASignature/test.py
@@ -1,7 +1,7 @@
 import composant_BIP39Converter as bip39
 
 converter = bip39.BIP39Converter()
-
+print("here")
 private_key = "4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0"
 mnemonic = converter.privateKeyToMnemonic(private_key)
 print(mnemonic)
