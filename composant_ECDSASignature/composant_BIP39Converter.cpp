0e7149168d47b4e05d22888d023edc8fe4320a47
samba914 <45944998+samba914@users.noreply.github.com>
2023-09-22 22:09:36 +0200

    Update composant_BIP39Converter.cpp

--- composant_ECDSASignature/composant_BIP39Converter.cpp
+++ composant_ECDSASignature/composant_BIP39Converter.cpp
@@ -19,29 +19,26 @@ const char *getVersion()
 }
 
 
-
-namespace py = pybind11;
-
 class BIP39Converter
 {
 public:
     std::string privateKeyToMnemonic(const std::string &privateKeyHex)
     {
-        // Convert hex string to bytes
         uint8_t data[32];
         for (int i = 0; i < 32; i++)
             sscanf(privateKeyHex.c_str() + 2*i, "%02hhx", &data[i]);
             
-        // Convert to mnemonic
         const char *mnemonic = mnemonic_from_data(data, sizeof(data));
         return std::string(mnemonic);
     }
 
     std::string mnemonicToSeed(const std::string &mnemonic)
     {
-        uint8_t seed[512 / 8];
-        const int seed_size = mnemonic_to_seed(mnemonic.c_str(), "", seed, NULL);
-        
+        const int seed_size = 512 / 8;  // Assuming seed size is always 512/8 bytes
+        uint8_t seed[seed_size];
+
+        mnemonic_to_seed(mnemonic.c_str(), "", seed, NULL);
+
         char hexseed[2 * seed_size + 1];
         for(int i = 0; i < seed_size; i++)
             sprintf(hexseed + 2*i, "%02x", seed[i]);
